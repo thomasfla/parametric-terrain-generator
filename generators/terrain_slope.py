7c3367e52aae3b7f976252648be8d70f0afc4f2d
Pierre-Alexandre Leziart <paleziart@laas.fr>
2024-02-16 13:47:39 +0100

    Clean slope terrain

--- generators/terrain_slope.py
+++ generators/terrain_slope.py
@@ -27,49 +27,6 @@ def create_square_plane(center, width, z_height):
     return trimesh.Trimesh(vertices=corners, faces=faces)
 
 
-def create_wall(center, size, height, step_height):
-    """
-    Generate a squared wall mesh with a specified base center, size, and height.
-
-    Parameters:
-    - center (tuple/list of float): Center of the base of the wall.
-    - size (float): Length and width of the wall base.
-    - height (float): Base height of the wall.
-    - step_height (float): Additional height for the top vertices of the wall.
-
-    Returns:
-    - trimesh.Trimesh: Mesh object representing the wall.
-    """
-    half_size = size / 2
-    base_z = height
-    top_z = height + step_height
-    vertices = np.array(
-        [
-            [center[0] - half_size, center[1] - half_size, base_z],
-            [center[0] + half_size, center[1] - half_size, base_z],
-            [center[0] + half_size, center[1] + half_size, base_z],
-            [center[0] - half_size, center[1] + half_size, base_z],
-            [center[0] - half_size, center[1] - half_size, top_z],
-            [center[0] + half_size, center[1] - half_size, top_z],
-            [center[0] + half_size, center[1] + half_size, top_z],
-            [center[0] - half_size, center[1] + half_size, top_z],
-        ]
-    )
-    faces = np.array(
-        [
-            [0, 1, 5],
-            [0, 5, 4],
-            [1, 2, 6],
-            [1, 6, 5],
-            [2, 3, 7],
-            [2, 7, 6],
-            [3, 0, 4],
-            [3, 4, 7],
-        ]
-    )
-    return trimesh.Trimesh(vertices=vertices, faces=faces)
-
-
 def create_step_top(center, outer_size, inner_size, height):
     """
     Generate a step top mesh, a square plane with a square hole.
@@ -112,48 +69,20 @@ def create_step_top(center, outer_size, inner_size, height):
     return trimesh.Trimesh(vertices=vertices, faces=faces)
 
 
-def generate_smooth_slope_side(center, yaw, outer_size, inner_size, height):
-    half_outer = outer_size / 2
-    half_inner = inner_size / 2
-    vertices = np.array(
-        [
-            [center[0] + half_outer, center[1] - half_outer, 0.0],
-            [center[0] + half_outer, center[1] + half_outer, 0.0],
-            [center[0] + half_inner, center[1] + half_inner, height],
-            [center[0] + half_inner, center[1] - half_inner, height],
-        ]
-    )
-    faces = np.array(
-        [
-            [0, 1, 2],
-            [2, 3, 0],
-        ]
-    )
-
-    mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
-
-    # Place the mesh in the world
-    rot_matrix = trimesh.transformations.rotation_matrix(
-        yaw, [0, 0, 1], [center[0], center[1], 0]
-    )
-    mesh.apply_transform(rot_matrix)
-
-    return mesh
-
-
 def create_bar(center, alpha, outer_size, inner_size, bar_height, bar_width):
     """
     Generate a block mesh centered at a given point with a given orientation.
 
     Parameters:
     - center (tuple/list of float): Center point (x, y) of the block.
-    - yaw (float): Orientation in yaw.
-    - length (float): Size of the block along the first axis.
-    - width (float): Size of the block along the second axis.
-    - block_height (float): Height of the block.
+    - alpha (float): Relative position on the slope between 0 and 1.
+    - outer_size (float): Size of the outer square.
+    - inner_size (float): Size of the inner square (hole).
+    - bar_height (float): Width of the transversal bars on the slope.
+    - bar_width (float): Height of the transversal bars on the slope.
 
     Returns:
-    - trimesh.Trimesh: Mesh object representing the block.
+    - trimesh.Trimesh: Mesh object representing the bar.
     """
     half_outer = outer_size / 2
     half_inner = inner_size / 2
@@ -207,9 +136,65 @@ def create_bar(center, alpha, outer_size, inner_size, bar_height, bar_width):
     return mesh
 
 
+def generate_smooth_slope_side(center, yaw, outer_size, inner_size, height):
+    """
+    Generate a smooth slope centered at a given point with a given orientation.
+
+    Parameters:
+    - center (tuple/list of float): Center point (x, y) of the block.
+    - yaw (float): Orientation in yaw.
+    - outer_size (float): Size of the outer square.
+    - inner_size (float): Size of the inner square (hole).
+    - height (float): Z-coordinate (height) of the plane.
+
+    Returns:
+    - trimesh.Trimesh: Mesh object representing the slope.
+    """
+    half_outer = outer_size / 2
+    half_inner = inner_size / 2
+    vertices = np.array(
+        [
+            [center[0] + half_outer, center[1] - half_outer, 0.0],
+            [center[0] + half_outer, center[1] + half_outer, 0.0],
+            [center[0] + half_inner, center[1] + half_inner, height],
+            [center[0] + half_inner, center[1] - half_inner, height],
+        ]
+    )
+    faces = np.array(
+        [
+            [0, 1, 2],
+            [2, 3, 0],
+        ]
+    )
+
+    mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+
+    # Place the mesh in the world
+    rot_matrix = trimesh.transformations.rotation_matrix(
+        yaw, [0, 0, 1], [center[0], center[1], 0]
+    )
+    mesh.apply_transform(rot_matrix)
+
+    return mesh
+
+
 def generate_barred_slope_side(
     center, yaw, outer_size, inner_size, height, bar_height, bar_width
 ):
+    """
+    Generate a slope with bars centered at a given point with a given orientation.
+
+    Parameters:
+    - center (tuple/list of float): Center point (x, y) of the block.
+    - yaw (float): Orientation in yaw.
+    - outer_size (float): Size of the outer square.
+    - inner_size (float): Size of the inner square (hole).
+    - bar_height (float): Width of the transversal bars on the slope.
+    - bar_width (float): Height of the transversal bars on the slope.
+
+    Returns:
+    - trimesh.Trimesh: Mesh object representing the slope.
+    """
     half_outer = outer_size / 2
     half_inner = inner_size / 2
     vertices = np.array(
@@ -254,8 +239,11 @@ def generate_barred_slope_side(
     )
 
     # Correction along Z
-    maxz = np.max(mesh.vertices[:, 2] - height)
-    coeff = -height / maxz
+    if height < 0:
+        minmaxz = np.max(mesh.vertices[:, 2] - height)
+    else:
+        minmaxz = np.min(mesh.vertices[:, 2] - height)
+    coeff = -height / minmaxz
     mesh.vertices[:, 2] = coeff * (mesh.vertices[:, 2] - height) + height
 
     # Place the mesh in the world
@@ -268,58 +256,72 @@ def generate_barred_slope_side(
 
 
 def generate_slope_terrain(
-    terrain_size=8.0, step_width=0.2, step_height=0.08, platform_size=1.0, going_up=True
+    terrain_size=8.0,
+    total_height=1.0,
+    bar_height=0.2,
+    bar_width=0.2,
+    platform_size=1.0,
+    going_up=True,
 ):
     """
-    Generate a mesh for a pyramid stairs terrain.
+    Generate a mesh for a sloped terrain.
 
     Parameters:
     - terrain_size (float): Size of the terrain.
-    - step_width (float): Width of each step.
-    - step_height (float): Height of each step.
+    - total_height (float): Total height of the slope.
+    - bar_height (float): Width of the transversal bars on the slope.
+    - bar_width (float): Height of the transversal bars on the slope.
     - platform_size (float): Size of the central platform.
-    - going_up (bool): Direction of the stairs, True for going up.
+    - going_up (bool): Direction of the slope, True for going up.
 
     Returns:
     - trimesh.Trimesh: Mesh object representing the pyramid stairs terrain.
     """
     center = [terrain_size / 2.0, terrain_size / 2.0]
-    num_steps = int(((terrain_size - platform_size) / 2.0) / step_width)
-    total_height = num_steps * step_height
     sign = 1 if going_up else -1
     meshes = []
 
     # Central platform
     meshes.append(create_square_plane(center, platform_size, -sign * total_height))
 
+    # North slope
     slope_mesh = generate_smooth_slope_side(
         center, 0.0, terrain_size - 2.0, platform_size, -sign * total_height
     )
     meshes.append(slope_mesh)
 
+    # West slope
     barred_slope_mesh = generate_barred_slope_side(
         center,
         np.pi / 2,
         terrain_size - 2.0,
         platform_size,
         -sign * total_height,
-        0.1,
-        0.2,
+        bar_height,
+        bar_width,
+    )
+    meshes.append(barred_slope_mesh)
+
+    # South slope
+    slope_mesh = generate_smooth_slope_side(
+        center, np.pi, terrain_size - 2.0, platform_size, -sign * total_height
+    )
+    meshes.append(slope_mesh)
+
+    # East slope
+    barred_slope_mesh = generate_barred_slope_side(
+        center,
+        3 * np.pi / 2,
+        terrain_size - 2.0,
+        platform_size,
+        -sign * total_height,
+        bar_height,
+        bar_width,
     )
     meshes.append(barred_slope_mesh)
 
     step_mesh = create_step_top(center, terrain_size, terrain_size - 2.0, 0.0)
     meshes.append(step_mesh)
-
-    """for i in range(num_steps):
-        outer_size = platform_size + 2 * step_width * (i + 1)
-        outer_size = terrain_size if i == num_steps - 1 else outer_size
-        inner_size = platform_size + 2 * step_width * i
-        height = sign * (step_height * (i + 1) - total_height)
-        step_mesh = create_step_top(center, outer_size, inner_size, height)
-        wall_mesh = create_wall(center, inner_size, height, -sign * step_height)
-        meshes.append(step_mesh)
-        meshes.append(wall_mesh)"""
 
     return trimesh.util.concatenate(meshes)
 
